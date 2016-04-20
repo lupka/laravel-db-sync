ac05f5dac274ab726837544f11bd6f94ed565258
Alex Chalupka <lupka31@gmail.com>
2016-04-20 17:08:42 -0500

    some progress but not quite working, may have to dump to file instead

--- src/Commands/DbSyncCommand.php
+++ src/Commands/DbSyncCommand.php
@@ -4,6 +4,8 @@ namespace Lupka\LaravelDbSync\Commands;
 
 use Illuminate\Console\Command;
 use Collective\Remote\RemoteFacade as SSH;
+use Symfony\Component\Process\Process; // TODO: require this somehow?
+use DB;
 
 class DbSyncCommand extends Command
 {
@@ -42,10 +44,32 @@ class DbSyncCommand extends Command
 
         $this->line('Logging into '.$connection);
 
+        // grab the dump data
+        $sqlDump = '';
         SSH::into($connection)->run([
             'cd '.$this->getRemoteDirectory($connection),
-            'php artisan',
-        ]);
+            'php artisan db:dump',
+        ],
+        function($line) use (&$sqlDump){
+            $sqlDump .= $line;
+        });
+
+        $config = config('database.connections.mysql');
+        $command = $this->getMysqlImportCommand($config, $sqlDump);
+        //echo $command; die();
+        $process = new Process($command);
+		//$process->setTimeout(600); // 10 minutes, should be more?
+		$process->run();
+
+        if ($process->isSuccessful())
+		{
+            $this->line($process->getOutput());
+		}
+		else
+		{
+			$this->line($process->getErrorOutput());
+		}
+
     }
 
     /**
@@ -70,5 +94,28 @@ class DbSyncCommand extends Command
     protected function getRemoteDirectory($connection)
     {
         return config('remote.connections.'.$connection.'.path');
+    }
+
+
+    /**
+     * Get import command
+     *
+     * @return string
+     */
+    public function getMysqlImportCommand($config, $sqlDump)
+    {
+        $command = sprintf('mysql --user=%s --password=%s %s < %s',
+			escapeshellarg($config['username']),
+			escapeshellarg($config['password']),
+			escapeshellarg($config['database']),
+            $sqlDump
+		);
+        // $command = sprintf('mysql --user=%s --password=%s %s < %s',
+		// 	escapeshellarg($config['username']),
+		// 	escapeshellarg($config['password']),
+		// 	escapeshellarg($config['database']),
+        //     $sqlDump
+		// );
+		return $command;
     }
 }
